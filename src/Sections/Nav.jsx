e16aa4d40ee2b315da2f1b7e55433eb698149228
Alex Kir <alexkir2304@gmail.com>
2025-07-10 17:51:37 +0300

    Developing...

--- src/Sections/Nav.jsx
+++ src/Sections/Nav.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {navigation} from "../constants/index.js";
+
 
 const Nav = () => {
     return (
@@ -9,8 +11,30 @@ const Nav = () => {
                 </h1>
             </div>
 
-            <div className='w-[50vw]'>
-                poshla jara
+            <div className='flex justify-around gap-0 w-[50vw] '>
+                {navigation.map((item, index) => (
+                    <div key={index} className='relative'>
+                        <div>
+                            <a href={item.link}>{item.title}</a>
+                        </div>
+                        <div className='flex absolute fixed top-[100px] left-0 w-[100vw]'>
+                            {item.menuSectors && item.menuSectors.map((sector, index) => (
+                                <div key={index} className='flex flex-col w-1/4'>
+                                    <div>{sector.title}</div>
+                                    <div>{sector.text && sector.text}</div>
+                                    <div className='flex flex-col'>{sector.links && sector.links.map((link, index) => (
+                                        <a key={index} href={link.url}>{link.title}</a>
+                                    ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+
+                <div>
+                    HAMBURGER
+                </div>
             </div>
 
             <div className='w-[25vw]'>
